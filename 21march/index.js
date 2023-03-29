33e91293ced7e467d2beb55decd5fa5466e0fd40
Tanushi Solanky <tanushi.solanky@1Rintern.local>
2023-03-29 17:56:23 +0530

    T

--- 21march/index.js
+++ 21march/index.js
@@ -1,83 +1,100 @@
-// switch case to display day
+// // switch case to display day
 
-const day=4;
-switch (day) {
-    case 0:
-        console.log("today is Sunday");
-        break;
-    case 1:
-        console.log("today is Monday");
-        break; 
-    case 2:
-        console.log("today is Tuesday");
-        break;  
-    case 3:
-        console.log("today is Wednesday");
-        break; 
-    case 4:
-        console.log("today is Thursday");
-        break;
-    case 5:
-        console.log("today is Friday");
-        break;   
-    case 6:
-        console.log("today is Saturday");
-        break;
-}
+// const day=4;
+// switch (day) {
+//     case 0:
+//         console.log("today is Sunday");
+//         break;
+//     case 1:
+//         console.log("today is Monday");
+//         break; 
+//     case 2:
+//         console.log("today is Tuesday");
+//         break;  
+//     case 3:
+//         console.log("today is Wednesday");
+//         break; 
+//     case 4:
+//         console.log("today is Thursday");
+//         break;
+//     case 5:
+//         console.log("today is Friday");
+//         break;   
+//     case 6:
+//         console.log("today is Saturday");
+//         break;
+// }
 
 
 // question 1
 
 
-// question 2
 
-let num = 5;
-let prime = isPrime(num);
+function reverseNumber(num) {
+  let reversed = 0;
 
-function isPrime(num) {
-    if (num < 2) {
-      return console.log("not a prime no.");
-    }
-    if (num === 2) {
-      return console.log(" prime no.");
-    }
-    if (num % 2 === 0) {
-        return console.log("not a prime no.");
-      }
-    for (let i = 3; i <= Math.sqrt(num); i += 2) {
-      if (num % i === 0) {
-        return console.log("not a prime no.");
-      }
-    }
-    return console.log(" prime no.");
+  while (num !== 0) {
+    const digit = num % 10;
+    reversed = reversed * 10 + digit;
+    num = Math.trunc(num / 10);
   }
 
-
-  // question 3
-
-function type(arg) {
-  return typeof arg;
+  return reversed;
 }
-  let string = "tanushi";
-  let number = 42;
-  let object = { 
-      name: "tanushi", 
-      age: 30,
-      gender: "female" 
-  };
-
-  console.log(type(string));
-  console.log(type(number)); 
-  console.log(type(object)); 
+const num = 123;
+const reversedNum = reverseNumber(num);
+console.log(reversedNum); 
 
 
-  // question 4
+// question 2
 
-const str= "tanushi solanky";
-const strlength = str.length;
-for (let i = strlength-1; i>=0; i--){
-    console.log(str[i]);
-}
+// let num = 5;
+// let prime = isPrime(num);
+
+// function isPrime(num) {
+//     if (num < 2) {
+//       return console.log("not a prime no.");
+//     }
+//     if (num === 2) {
+//       return console.log(" prime no.");
+//     }
+//     if (num % 2 === 0) {
+//         return console.log("not a prime no.");
+//       }
+//     for (let i = 3; i <= Math.sqrt(num); i += 2) {
+//       if (num % i === 0) {
+//         return console.log("not a prime no.");
+//       }
+//     }
+//     return console.log(" prime no.");
+//   }
+
+
+//   // question 3
+
+// function type(arg) {
+//   return typeof arg;
+// }
+//   let string = "tanushi";
+//   let number = 42;
+//   let object = { 
+//       name: "tanushi", 
+//       age: 30,
+//       gender: "female" 
+//   };
+
+//   console.log(type(string));
+//   console.log(type(number)); 
+//   console.log(type(object)); 
+
+
+//   // question 4
+
+// const str= "tanushi solanky";
+// const strlength = str.length;
+// for (let i = strlength-1; i>=0; i--){
+//     console.log(str[i]);
+// }
 
 
 
--- table/index.js
+++ table/index.js
@@ -1,31 +1,35 @@
 const table = document.getElementById('table');
 
-const tableHead = ['NAME', 'AGE', 'EMAIL'];
-const row1 = ['Tanushi', '21', 't@gmail.com'];
-const row2 = ['Solanky', '22', 's@gmail.com'];
-const row3 = ['Bhargav', '25', 'b@gmail.com'];
+const tableHead = [{name :'NAME'}, {age :'AGE'}, {email : 'EMAIL'}];
+const row1 = [{name :'Tanushi' , age :'21' , email : 't@gmail.com'}];
+// const row2 = ['Solanky', '22', 's@gmail.com'];
+// const row3 = ['Bhargav', '25', 'b@gmail.com'];
 
 
 const tHead = table.createTHead();
 const thRow = tHead.insertRow();
 tableHead.forEach(element => {
+    for (const key in element){
     const td = thRow.insertCell();
-    td.appendChild(document.createTextNode(element));
+    td.appendChild(document.createTextNode(Object.values(element)));
+    }
 });
 
 const tBody = table.createTBody();
 const td1Row = tBody.insertRow();
 row1.forEach(element => {
+    for (const key of tableHead){
     const td1 = td1Row.insertCell();
-    td1.appendChild(document.createTextNode(element));
+    td1.appendChild(document.createTextNode(element[Object.keys(key)]));
+    }
 });
-const td2Row = tBody.insertRow();
-row2.forEach(element => {
-    const td2 = td2Row.insertCell();
-    td2.appendChild(document.createTextNode(element));
-});
-const td3Row = tBody.insertRow();
-row3.forEach(element => {
-    const td3 = td3Row.insertCell();
-    td3.appendChild(document.createTextNode(element));
-});
+// const td2Row = tBody.insertRow();
+// row2.forEach(element => {
+//     const td2 = td2Row.insertCell();
+//     td2.appendChild(document.createTextNode(element));
+// });
+// const td3Row = tBody.insertRow();
+// row3.forEach(element => {
+//     const td3 = td3Row.insertCell();
+//     td3.appendChild(document.createTextNode(element));
+// });
